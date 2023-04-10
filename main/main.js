0f514f8ff951004c6309df4117dd332119ddf427
Nicolas Rodriguez <rodriguez.supervielle.nicolas@gmail.com>
2023-04-10 16:34:24 -0300

    JavaScript

---
+++ main/main.js
@@ -0,0 +1,41 @@
+let nombre =prompt("Hola, ingresa tu nombre y apellido");
+
+if (nombre != "") {
+    alert("Hola" + " " + nombre +" " + "bienvenido");
+}else{
+    prompt("Su nombre no puede estar vacio.\nPor favor indique su nombre y apalledio")
+}
+
+let comprar="1";
+let vender="2";
+let salir="3";
+let eleccion;
+
+do{
+    eleccion = prompt("Ingresa 1 si queres comprar\nIngresa 2 si quieres vender\nIngresa 3 si quieres salir")
+    if(eleccion == comprar){ compra()
+}else if(eleccion == vender){ venta()
+}else if(eleccion == salir){
+    alert("Muchas gracias, te esperamos la proxima")}
+else{
+    alert("Tu respuesta no es correcta , vuelve a ingresar e ingresa una respuesta correcta")
+}
+} while (eleccion != salir);
+
+
+function compra(){
+    let cantidadCompra= parseInt(prompt("Cuantos dolares desea comprar\nIndique el valor en numeros"));
+    if(Number.isInteger(cantidadCompra)){
+     alert("El costo total de la operacion es de:"+(390 * cantidadCompra + " " + "pesos argentinos"));
+    }else{
+        alert("Por favor vuelva a repetir la operacion e ingrese un valor numerico");
+    }
+}
+function venta(){
+    let cantidadVenta= parseInt (prompt("Cuantos dolares desea vender? Indique el valor en numeros"));
+    if(Number.isInteger(cantidadVenta)){
+      alert("Le damos un total de  : "+(cantidadVenta * 380 + " " + "pesos argentinos"));
+    }else{
+        alert("Por favor vuelva a repetir la operacion e ingrese un valor numerico")
+    }
+}
