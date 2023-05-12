55272f7c490ad3a7c5c659db1f67b3190fd71de2
Nicolas Rodriguez <rodriguez.supervielle.nicolas@gmail.com>
2023-05-12 13:29:29 -0300

    terceraPreEntregaCoder

--- main/main.js
+++ main/main.js
@@ -1,51 +1,60 @@
 const formulario = document.getElementById("formulario");
 
-formulario.addEventListener("submit", enviarFormulario)
+formulario.addEventListener("submit",(e) => {
+const nombre = document.querySelector("#nombre").value;
+const apellido = document.querySelector("#apellido").value;
+const dni = document.querySelector("#dni").value;
+let cantidad = document.getElementById("cantidad").value;
 
-function enviarFormulario(e){
-    e.preventDefault();
-    const nombre= document.querySelector("#nombre").value;
-    if(nombre ==""){
-        alert("Su nombre no puede estar vacio");
-        document.getElementById("nombre").focus();
-    }else{
-    document.getElementById("nombre").value ="";
+if((nombre == "" || apellido == "" || dni.length < 8 || cantidad =="")){
+    alert ("No puede dejar espacios sin rellenar")
+} else {
+    const usuarioOperacion ={
+        nombre: nombre, 
+        apellido: apellido,
+        dni: dni,
+    }
+    enviarFormulario(e,cantidad,usuarioOperacion)
 }
-    const apellido = document.querySelector("#apellido").value;
-    if(apellido ==""){
-        alert("Su apellido no puede estar vacio");
-        document.getElementById("apellido").focus();
-    }else{
-        document.getElementById("apellido").value ="";
-    }
+})
 
-    const dni = document.querySelector("#dni").value;
-    if(dni.length < 8 ){
-    alert("Ingrese su DNI ");
-    document.getElementById("dni").focus();
-    }else{
-    document.getElementById("dni").value ="";
-    };
+function enviarFormulario (e, cantidad, usuarioOperacion){
+    e.preventDefault();
 
-    let cantidad = document.getElementById("cantidad").value;
-
-    if(document.getElementById("compra").checked){comprar()
-    }
-    else if(document.getElementById("venta").checked){vender()
-    };    
+    const mostrarInfo = document.getElementById("mostrarInfo")
+    let inputCompra = document.getElementById("compra")
+    let inputVenta = document.getElementById("venta")
+    // let inputSucursal1 = document.getElementById("sucursal1")
+    // let inputSucursal2 = document.getElementById("sucursal2")
+    // let inputSucursal3 = document.getElementById("sucursal3")
+    // let inputSucursal4 = document.getElementById("sucursal4")
     
-    function comprar(){
-        let cantidad = document.getElementById("cantidad").value;
-        cantidad = 400 * cantidad;
-        document.getElementById("cantidad").value="";
-    };
-
-    function vender(){
-        let cantidad = document.getElementById("cantidad").value;
-        console.log(400 * cantidad);
-        document.getElementById("cantidad").value="";
+    if(inputCompra.checked){
+        cantidadOperacion = comprar(cantidad)
+        mostrarInfo.innerHTML = `Su operacion fue realizada con exito,tienes 24hs para acercarte a la sucursal seleccionada y finalizar la operacion.No olvides presentar tu DNI. <br/> <br/> Nombre y Apellido: ${usuarioOperacion.nombre} ${usuarioOperacion.apellido} <br/> DNI: ${usuarioOperacion.dni}<br/> El valor total de la operacion es de : ${cantidadOperacion} pesos Argentinos  `
+    } else if (inputVenta.checked){
+        cantidadOperacion = vender(cantidad)
+        mostrarInfo.innerHTML = `Su operacion fue realizada con exito,tienes 24hs para acercarte a la sucursal seleccionada y finalizar la operacion  <br/> <br/> Nombre y Apellido: ${usuarioOperacion.nombre} ${usuarioOperacion.apellido} <br/> DNI: ${usuarioOperacion.dni}<br/> Recibe un total de : ${cantidadOperacion} pesos Argentinos  `
     }
+
+    // if(inputSucursal1.checked)
+    // else(inputSucursal2.checked)
+    // else(inputSucursal3.checked)
+    // else if(inputSucursal4.checked)
     
+    
+    formulario.reset()
+};
+
+function comprar (cantidad){
+    cantidad = 480*cantidad;
+    return cantidad
+};
+
+function vender (cantidad){
+    cantidad = 420*cantidad;
+    return cantidad 
+}
 
 /*const sucursales =[
     {nombre:"Sucursal Balvanera",direccion:"Avenida Belgrano 2590",horario:"09 a 16hs"},
@@ -54,8 +63,3 @@ function enviarFormulario(e){
     {nombre:"Sucursal Ramos Mejia ",direccion:"Avenida de Mayo 1364",horario:"11 a 16:30hs"}]
     
 */
-
-const mostrarInfo = document.getElementById("mostrarInfo")
-mostrarInfo.innerHTML = `Su operacion fue realizada con exito,tienes 24hs para acercarte a la sucursal seleccionada y finalizar la operacion  <br/> <br/> Nombre y Apellido: ${nombre} ${apellido} <br/> DNI: ${dni}<br/> Recibe una cantidad de : ${cantidad}   `
-    
-};
