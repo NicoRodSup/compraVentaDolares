e21a4d5691d0a6451c975706179a8b91dccd0524
Nicolas Rodriguez <rodriguez.supervielle.nicolas@gmail.com>
2023-05-12 08:56:12 -0300

    terceraPreEntrega

--- main/main.js
+++ main/main.js
@@ -1,85 +1,61 @@
-let eleccion;
-let comprar;
-let vender;
-let continuar;
+const formulario = document.getElementById("formulario");
 
-let nombre =prompt("Hola, ingresa tu nombre y apellido");
+formulario.addEventListener("submit", enviarFormulario)
 
-const sucursales =[
+function enviarFormulario(e){
+    e.preventDefault();
+    const nombre= document.querySelector("#nombre").value;
+    if(nombre ==""){
+        alert("Su nombre no puede estar vacio");
+        document.getElementById("nombre").focus();
+    }else{
+    document.getElementById("nombre").value ="";
+}
+    const apellido = document.querySelector("#apellido").value;
+    if(apellido ==""){
+        alert("Su apellido no puede estar vacio");
+        document.getElementById("apellido").focus();
+    }else{
+        document.getElementById("apellido").value ="";
+    }
+
+    const dni = document.querySelector("#dni").value;
+    if(dni.length < 8 ){
+    alert("Ingrese su DNI ");
+    document.getElementById("dni").focus();
+    }else{
+    document.getElementById("dni").value ="";
+    };
+
+    let cantidad = document.getElementById("cantidad").value;
+
+    if(document.getElementById("compra").checked){comprar()
+    }
+    else if(document.getElementById("venta").checked){vender()
+    };    
+    
+    function comprar(){
+        let cantidad = document.getElementById("cantidad").value;
+        cantidad = 400 * cantidad;
+        document.getElementById("cantidad").value="";
+    };
+
+    function vender(){
+        let cantidad = document.getElementById("cantidad").value;
+        console.log(400 * cantidad);
+        document.getElementById("cantidad").value="";
+    }
+    
+
+/*const sucursales =[
     {nombre:"Sucursal Balvanera",direccion:"Avenida Belgrano 2590",horario:"09 a 16hs"},
     {nombre:"Sucursal Devoto",direccion:"Cuenca 3420",horario:"10 a 17:30hs"},
     {nombre:"Sucursal Almagro",direccion:"Avenida Rivadavia 4500",horario:"10 a 15:30hs"},
     {nombre:"Sucursal Ramos Mejia ",direccion:"Avenida de Mayo 1364",horario:"11 a 16:30hs"}]
-
-if (nombre != "") {
-    alert("Hola" + " " + nombre +" " + "bienvenido");
-    do{
-        eleccion = prompt("Ingresa 1 si queres comprar\nIngresa 2 si quieres vender\nIngresa 3 si quieres finalizar")
-        comprar=1
-        vender=2
-        finalizar=3
-        if(eleccion == comprar){ compra()
-    }else if(eleccion == vender){ venta() 
-    }else if(eleccion == finalizar){
-        alert("Aguarda que finalizamos la operacion")}
-    else{
-        alert("Tu respuesta no es correcta ,ingresa una respuesta correcta")
-    }
-    } while (eleccion != finalizar);
     
+*/
 
-}else{
-    prompt("Su nombre no puede estar vacio.\nPor favor indique su nombre y apalledio")
-}
-
-function compra(){
-    let cantidadCompra= parseInt(prompt("Cuantos dolares desea comprar\nIndique el valor en numeros"));
-    if(Number.isInteger(cantidadCompra)){
-     alert("El costo total de la operacion es de:"+(390 * cantidadCompra + " " + "pesos argentinos.\nA continuacion le vamos a indicar donde puede finalizar la operaion al precio acordado"));
-    const nombreSucursales = sucursales.map(sucursal => sucursal.nombre)
-    const mensaje = `Nombre de las sucursales: \n${nombreSucursales.join("\n")}`
-    alert(mensaje);
-    }else{
-        alert("Por favor vuelva a repetir la operacion e ingrese un valor numerico");
-    }
-}
-
-function venta(){
-    let cantidadVenta= parseInt (prompt("Cuantos dolares desea vender? Indique el valor en numeros"));
-    if(Number.isInteger(cantidadVenta)){
-      alert("Le damos un total de  : "+(cantidadVenta * 380 + " " + "pesos argentinos\nA continuacion le vamos a indicar donde puede finalizar la operacion al precio acordado"));
-    const nombreSucursales = sucursales.map(sucursal => sucursal.nombre)
-    const mensaje = `Nombre de las sucursales: \n${nombreSucursales.join("\n")}`
-    alert (mensaje);
-    }else{
-        alert("Por favor vuelva a repetir la operacion e ingrese un valor numerico")
-    }
-}
-
-
-//ARRAYS CON LOS CLIENTES
-class Cliente {
-    constructor(nombre, direccion, dni) {
-        this.nombre  = nombre;
-        this.direccion  = direccion;
-        this.dni = dni;
-        this.info= `Nombre: ${this.nombre}\nDireccion: ${this.direccion}\n DNI: ${this.dni}`;
-    }
-}
-//Declaramos un array de clientes para almacenar objetos
-const clientes = [];
-//push agregamos clientes
-clientes.push(new Cliente("Nicolas Rodriguez","Campana 3557", "36807471"));
-clientes.push(new Cliente("Romina Corda","Cullen 5134", "33409781"));
-clientes.push(new Cliente("Martin Lutfi","Delgado 331" ,"28457162"));
-
-// para ver la cantidad de clientes que hay //
-console.log(Cliente.length);
-
-//mostrar nombre de un cliente 
-
-clientes.forEach(cliente => {
-        if(cliente.nombre.includes("Nicolas Rodriguez")){
-            console.log(`Se encontro al cliente "${cliente.nombre}"`);
-        }
-    });
+const mostrarInfo = document.getElementById("mostrarInfo")
+mostrarInfo.innerHTML = `Su operacion fue realizada con exito,tienes 24hs para acercarte a la sucursal seleccionada y finalizar la operacion  <br/> <br/> Nombre y Apellido: ${nombre} ${apellido} <br/> DNI: ${dni}<br/> Recibe una cantidad de : ${cantidad}   `
+    
+};
---
+++ script.js
@@ -0,0 +1,86 @@
+/*let eleccion;
+let comprar;
+let vender;
+let continuar;
+
+let nombre =prompt("Hola, ingresa tu nombre y apellido");
+
+const sucursales =[
+    {nombre:"Sucursal Balvanera",direccion:"Avenida Belgrano 2590",horario:"09 a 16hs"},
+    {nombre:"Sucursal Devoto",direccion:"Cuenca 3420",horario:"10 a 17:30hs"},
+    {nombre:"Sucursal Almagro",direccion:"Avenida Rivadavia 4500",horario:"10 a 15:30hs"},
+    {nombre:"Sucursal Ramos Mejia ",direccion:"Avenida de Mayo 1364",horario:"11 a 16:30hs"}]
+
+if (nombre != "") {
+    alert("Hola" + " " + nombre +" " + "bienvenido");
+    do{
+        eleccion = prompt("Ingresa 1 si queres comprar\nIngresa 2 si quieres vender\nIngresa 3 si quieres finalizar")
+        comprar=1
+        vender=2
+        finalizar=3
+        if(eleccion == comprar){ compra()
+    }else if(eleccion == vender){ venta() 
+    }else if(eleccion == finalizar){
+        alert("Aguarda que finalizamos la operacion")}
+    else{
+        alert("Tu respuesta no es correcta ,ingresa una respuesta correcta")
+    }
+    } while (eleccion != finalizar);
+    
+
+}else{
+    prompt("Su nombre no puede estar vacio.\nPor favor indique su nombre y apalledio")
+}
+
+function compra(){
+    let cantidadCompra= parseInt(prompt("Cuantos dolares desea comprar\nIndique el valor en numeros"));
+    if(Number.isInteger(cantidadCompra)){
+     alert("El costo total de la operacion es de:"+(390 * cantidadCompra + " " + "pesos argentinos.\nA continuacion le vamos a indicar donde puede finalizar la operaion al precio acordado"));
+    const nombreSucursales = sucursales.map(sucursal => sucursal.nombre)
+    const mensaje = `Nombre de las sucursales: \n${nombreSucursales.join("\n")}`
+    alert(mensaje);
+    }else{
+        alert("Por favor vuelva a repetir la operacion e ingrese un valor numerico");
+    }
+}
+
+function venta(){
+    let cantidadVenta= parseInt (prompt("Cuantos dolares desea vender? Indique el valor en numeros"));
+    if(Number.isInteger(cantidadVenta)){
+      alert("Le damos un total de  : "+(cantidadVenta * 380 + " " + "pesos argentinos\nA continuacion le vamos a indicar donde puede finalizar la operacion al precio acordado"));
+    const nombreSucursales = sucursales.map(sucursal => sucursal.nombre)
+    const mensaje = `Nombre de las sucursales: \n${nombreSucursales.join("\n")}`
+    alert (mensaje);
+    }else{
+        alert("Por favor vuelva a repetir la operacion e ingrese un valor numerico")
+    }
+}
+
+
+//ARRAYS CON LOS CLIENTES
+class Cliente {
+    constructor(nombre, direccion, dni) {
+        this.nombre  = nombre;
+        this.direccion  = direccion;
+        this.dni = dni;
+        this.info= `Nombre: ${this.nombre}\nDireccion: ${this.direccion}\n DNI: ${this.dni}`;
+    }
+}
+//Declaramos un array de clientes para almacenar objetos
+const clientes = [];
+//push agregamos clientes
+clientes.push(new Cliente("Nicolas Rodriguez","Campana 3557", "36807471"));
+clientes.push(new Cliente("Romina Corda","Cullen 5134", "33409781"));
+clientes.push(new Cliente("Martin Lutfi","Delgado 331" ,"28457162"));
+
+// para ver la cantidad de clientes que hay //
+console.log(Cliente.length);
+
+//mostrar nombre de un cliente 
+
+clientes.forEach(cliente => {
+        if(cliente.nombre.includes("Nicolas Rodriguez")){
+            console.log(`Se encontro al cliente "${cliente.nombre}"`);
+        }
+    });
+*/
